a87f4e4bf644e07a6c432b8542f756284cce2032
Natsuneko <me@mochizuki.moe>
2021-05-06 01:57:09 +0900

    chore: disable vitals on production

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,11 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+if (process.env.NODE_ENV === "development") {
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+  // eslint-disable-next-line no-console
+  reportWebVitals(console.log);
+}
